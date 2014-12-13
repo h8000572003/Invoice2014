1434925b0c6f9401d82b3d3a83de0c1f21732685
蔡 亞忠 <wa.addi@gmail.com>
2014-12-13 16:13:00 +0800

    New changelist

--- app/src/main/java/tw/com/wa/invoice/domain/Invoice.java
+++ app/src/main/java/tw/com/wa/invoice/domain/Invoice.java
@@ -1,57 +1,76 @@
 package tw.com.wa.invoice.domain;
 
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
 import java.io.Serializable;
 
 /**
  * Created by Andy on 14/12/9.
  */
-public class Invoice implements Serializable{
 
-    /**
-     * 獎項等級
-     */
-    private String awards;
+@ParseClassName("Invoice")
+public class Invoice extends ParseObject implements Serializable {
 
-    /**
-     * 發票號碼
-     */
-    private String number;
+    //    /**
+//     * 獎項等級
+//     */
+//    private String awards;
+//
+//    /**
+//     * 發票號碼
+//     */
+//    private String number;
+//
+//    /**
+//     *
+//     */
+//    private boolean specialize = false;
+    // private String title;
 
-    /**
-     *
-     */
-    private boolean specialize=false;
 
-
-    public Invoice(String awards, String number, boolean specialize) {
-        this.awards = awards;
-        this.number = number;
-        this.specialize = specialize;
-    }
+//    public Invoice(String awards, String number, boolean specialize) {
+//        this.awards = awards;
+//        this.number = number;
+//        this.specialize = specialize;
+//    }
 
     public boolean isSpecialize() {
-        return specialize;
+
+        return getBoolean("specialize");
     }
 
     public void setSpecialize(boolean specialize) {
-        this.specialize = specialize;
+        this.put("specialize", specialize);
+
     }
 
     public void setAwards(String awards) {
-        this.awards = awards;
+        this.put("awards", awards);
+
     }
 
     public String getNumber() {
-        return number;
+        return getString("number");
+
     }
 
     public void setNumber(String number) {
-        this.number = number;
+        put("number", number);
     }
 
     public String getAwards() {
-        return awards;
+        return getString("awards");
+
+
     }
 
+    public String getTitle() {
+        return getString("title");
+    }
 
+    public void setTitle(String title) {
+        this.put("title", title);
+
+    }
 }
